1accbff3766a3944ac8255e6702a1ec5c57ccfe4
esteban-legaspi <jomariux4@gmail.com>
2026-04-03 12:51:02 -0600

    Crear estructura inicial de proyecto WPF

---
+++ simulador-algoritmos-so/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace simulador_algoritmos_so
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
